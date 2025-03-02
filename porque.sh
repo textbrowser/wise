6fa917400ddcb37cb597d82957ed88e799a901f0
textbrowser <textbrowser@gmail.com>
2025-03-01 21:51:38 -0500

    Style.

--- porque.sh
+++ porque.sh
@@ -5,6 +5,7 @@
 export AA_ENABLEHIGHDPISCALING=1
 export AA_USEHIGHDPIPIXMAPS=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_QPA_PLATFORMTHEME=qt6ct
 export QT_X11_NO_MITSHM=1
 
 if [ -r ./Porque ] && [ -x ./Porque ]
