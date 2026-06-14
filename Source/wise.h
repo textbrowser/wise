c8d11984c162fdb8eb311d03deb835467ac1b774
textbrowser@gmail.com <textbrowser@gmail.com>
2026-06-14 11:35:15 -0400

    New version.

--- Source/wise.h
+++ Source/wise.h
@@ -32,7 +32,7 @@
 
 #include <QTimer>
 
-#define WISE_VERSION_STRING "2026.06.13"
+#define WISE_VERSION_STRING "2026.06.14"
 
 class wise_settings;
 typedef QVector<QPair<QImage, QString> > QVectorQPairQImageQString;
