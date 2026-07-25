ec11215428d8e5c7de1f1031fe6042f4d8f7e705
textbrowser@gmail.com <textbrowser@gmail.com>
2026-07-25 11:47:48 -0400

    Regular files.

--- wise.sh
+++ wise.sh
@@ -8,26 +8,36 @@ export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_QPA_PLATFORMTHEME=qt6ct
 export QT_X11_NO_MITSHM=1
 
-if [ -r ./Wise ] && [ -x ./Wise ]
+if [ -f ./Wise ] && [ -r ./Wise ] && [ -x ./Wise ]
 then
     echo "Launching a local Wise."
     ./Wise "$@"
     exit $?
-elif [ -r /opt/wise/Wise ] && [ -x /opt/wise/Wise ]
+fi
+
+if [ -f /opt/wise/Wise ] && [ -r /opt/wise/Wise ] && [ -x /opt/wise/Wise ]
 then
-    echo "Launching an official Wise."
+    echo "Launching an official Wise (/opt/wise)."
+
     export LD_LIBRARY_PATH=/opt/wise/Lib
     export QT_PLUGIN_PATH=/opt/wise/plugins
+
     /opt/wise/Wise "$@"
     exit $?
-elif [ -r /usr/local/wise/Wise ] && [ -x /usr/local/wise/Wise ]
+fi
+
+if [ -f /usr/local/wise/Wise ] && \
+   [ -r /usr/local/wise/Wise ] && \
+   [ -x /usr/local/wise/Wise ]
 then
-    echo "Launching an official Wise."
+    echo "Launching an official Wise (/usr/local/wise)."
+
     export LD_LIBRARY_PATH=/usr/local/wise/Lib
     export QT_PLUGIN_PATH=/usr/local/wise/plugins
+
     /usr/local/wise/Wise "$@"
     exit $?
-else
-    echo "Cannot locate Wise. Why?"
-    exit 1
 fi
+
+echo "Cannot locate Wise. Why?"
+exit 1
